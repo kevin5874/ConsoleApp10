b1083c9fb0c60692e8f90821b5e8ae2db608a680
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-07-09 16:22:35 -0500

    commit1

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer");
+            int x = Int32.Parse(Console.ReadLine());
+            doIntMath y = new doIntMath();
+            Console.WriteLine(y.addMath(x));
+            Console.WriteLine(y.minusMath(x));
+            Console.WriteLine(y.DivideMath(x));
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleApp10/doIntMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class doIntMath
+    {
+        public int addMath(int x)
+        {
+            x += 3;
+            return x;
+        }
+        public int minusMath(int x)
+        {
+            x -= 3;
+            return x;
+        }
+        public int DivideMath(int x)
+        {
+            x /= 2; // since we're using ints as parameter and return type this is going to return just the integer portion of the math operation
+            // for instance 3 /= 2 == 1 instead of 1.5
+            return x;
+        }
+    }
+}
